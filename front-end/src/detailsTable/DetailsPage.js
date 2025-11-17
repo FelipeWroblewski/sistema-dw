3633503aa217caf60cbd49885a97884f89f9ce44
Felipe Wroblewski <gabrielfelipewroblewski@gmail.com>
2025-11-16 21:03:44 -0300

    style: adjusting the layout of the table details screen

--- front-end/src/detailsTable/DetailsPage.js
+++ front-end/src/detailsTable/DetailsPage.js
@@ -6,12 +6,12 @@ function DetailsPage() {
     return (
         <div className='min-h-screen w-full bg-gray-100 dark:bg-[#1a1a1a]'>
             <NavBar />
-            <h1 className='pt-32 pl-48 text-2xl text-brancoPrincipal'>Nome da tabela: fpedidos_compra</h1>
+            <div class="w-4/5 mx-auto">
+                <h1 className='pt-32 text-start text-2xl text-black dark:text-[#E3CFAA]'>Nome da tabela: fpedidos_compra</h1>
+            </div>
 
             <div class="pt-5">
-            
-                <h1 class="w-4/5 mx-auto text-start text-4xl font-bold font-inter text-black dark:text-[#F5F2EB]"></h1> 
-                
+                            
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-14 w-4/5  mx-auto bg-white dark:bg-[#22211D] shadow-xl h-auto rounded-lg 
                     py-6 px-4 md:px-6 border border-gray-200 dark:border-gray-800 dark:border-none">
                         
